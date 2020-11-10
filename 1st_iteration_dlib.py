d905510a21412150a13cbe4c8e56f16f3f14cdfb
esha1995 <esha19@student.aau.dk>
2020-11-10 10:29:56 +0100

    1st iteration + video-feed

---
+++ 1st_iteration_dlib.py
@@ -0,0 +1,40 @@
+import cv2
+import dlib
+import numpy as np
+from imutils import face_utils
+
+smile_cascade = cv2.CascadeClassifier('OpenCV files/haarcascade_smile.xml')
+detector = dlib.get_frontal_face_detector()
+sp = dlib.shape_predictor('dlib files/shape_predictor_68_face_landmarks.dat')
+
+def detect(gray, frame):
+    faces = detector(gray, 1)
+    print("faces: " + str(len(faces)))
+    for (i, faces) in enumerate(faces):
+        shape = sp(gray, faces)
+        shape = face_utils.shape_to_np(shape)
+        (x, y, w, h) = face_utils.rect_to_bb(faces)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
+        for (x, y) in shape:
+            cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.2, 15)
+        print("smiles: " + str(len(smiles)))
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2)
+    return frame
+
+imagePath = 'images/smile'
+writePath = 'dlib_imagewrite'
+
+for i in range(24):
+    print("image number: " + str(i) + ": ")
+    cap = cv2.imread(imagePath+str(i)+'.jpg')
+    frame = cv2.resize(cap, (700, 512))
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+    canvas = detect(gray, frame)
+    filename = 'smileDetected' + str(i) + '.jpg'
+    cv2.imwrite(np.os.path.join(writePath, filename), canvas)
+
---
+++ 1st_iteration_opencv.py
@@ -0,0 +1,45 @@
+import cv2
+import dlib
+import matplotlib.pyplot as plt
+import numpy as np
+from imutils import face_utils
+
+smile_cascade = cv2.CascadeClassifier('OpenCV files/haarcascade_smile.xml')
+face_cascade = cv2.CascadeClassifier('OpenCV files/haarcascade_frontalface_default.xml')
+smileCounter = list()
+lookAwayCounter = list()
+
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5, minSize=(50,50))
+    print("faces: " + str(len(faces)))
+    if len(faces) < 1:
+        lookAwayCounter.append(1)
+    else:
+        lookAwayCounter.append(0)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.2, 15)
+        print("smiles: " + str(len(smiles)))
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2)
+        if len(smiles) > 0:
+            smileCounter.append(1)
+        else:
+            smileCounter.append(0)
+    return frame
+
+imagePath = 'images/smile'
+writePath = 'openCV_imagewrite'
+
+for i in range(24):
+    print("image number: " + str(i) + ": ")
+    cap = cv2.imread(imagePath+str(i)+'.jpg')
+    frame = cv2.resize(cap, (700, 512))
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+    canvas = detect(gray, frame)
+    filename = 'smileDetected' + str(i) + '.jpg'
+    cv2.imwrite(np.os.path.join(writePath, filename), canvas)
+
