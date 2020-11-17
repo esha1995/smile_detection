64444d7ac8c609b5e73bf7fe2073404a840264c9
Chikidik <33221343+Chikidik@users.noreply.github.com>
2020-11-17 14:50:25 +0100

    https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Media_Player_VLC_Based.py
    Added video player

--- dlib_webcam_guiTest.py
+++ dlib_webcam_guiTest.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import PySimpleGUI as sg
 from imutils import face_utils
+import vlc
 
 smile_cascade = cv2.CascadeClassifier('OpenCV files/haarcascade_smile.xml')
 detector = dlib.get_frontal_face_detector()
@@ -13,6 +14,9 @@ model = dlib.face_recognition_model_v1('dlib files/dlib_face_recognition_resnet_
 smileCounter = list()
 lookAwayCounter = list()
 framerate = 0
+
+def btn(name):  # a PySimpleGUI "User Defined Element" (see docs)
+    return sg.Button(name, size=(6, 1), pad=(1, 1))
 
 def detect(gray, frame,sf,mn):
     faces = detector(gray, 1)
@@ -38,37 +42,57 @@ def detect(gray, frame,sf,mn):
             cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2)
     return frame
 
+def calibrate(gray, frame,sf,mn):
+    faces = detector(gray, 1)
+    for (i, faces) in enumerate(faces):
+        shape = sp(gray, faces)
+        shape = face_utils.shape_to_np(shape)
+        (x, y, w, h) = face_utils.rect_to_bb(faces)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
+        for (x, y) in shape:
+            cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+        smiles = smile_cascade.detectMultiScale(roi_gray, sf, mn)
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2)
+    return frame
 
 def main():
-    sg.theme("LightGreen")
+    sg.theme("DarkBlue")
 
     layout = [
-        [sg.Text("OpenCV test", size=(60, 1), justification="center")],
+        [sg.Input(default_text='Video URL or Local Path:', size=(30, 1), key='-VIDEO_LOCATION-'), sg.Button('load')],
+        [sg.Text("OpenCV test", size=(60, 1), justification="left")],
         [sg.Image(filename="",key="-IMAGE-")],
-        [sg.Radio("None", "Radio", True, size=(10,1))],
-        [sg.Button("Start", size=(10, 1))],
-        [sg.Button("Exit", size=(10,1))],
+        [sg.Button("Start cal", size=(10, 1)),sg.Button("Stop cal",size=(10,1))],
+
+        [btn('previous'), btn('play'), btn('next'), btn('pause'), btn('stop')],
+        [sg.Text('Load media to start', key='-MESSAGE_AREA-')],
+        [sg.Text("Scale Factor", size=(60, 1), justification="left")],
         [
+
             sg.Slider(
                 (110, 200),
                 110,
                 5,
                 orientation="h",
-                size=(40, 15),
-                text="SCALE",
+                size=(20, 10),
                 key="-SCALE-",
             ),
         ],
+        [sg.Text("Neighbor", size=(60, 1), justification="left")],
         [
             sg.Slider(
                 (10, 20),
                 15,
                 1,
                 orientation="h",
-                size=(40, 15),
+                size=(20, 10),
                 key="-NEIGHB-",
             ),
         ],
+        [sg.Button("Exit", size=(10, 1))],
     ]
 
     while True:
@@ -79,9 +103,18 @@ def main():
         except:
             print("only numbers")
 
-    window = sg.Window("Test", layout, location=(800, 400))
+    window = sg.Window("Test", layout, element_justification='center', finalize=True, resizable=True)
+    window['-IMAGE-'].expand(True, True)
+    inst = vlc.Instance()
+    list_player = inst.media_list_player_new()
+    media_list = inst.media_list_new([])
+    list_player.set_media_list(media_list)
+    player = list_player.get_media_player()
+    player.set_hwnd(window['-IMAGE-'].Widget.winfo_id())
     cap = cv2.VideoCapture(webcam)
-    testBool = False
+    calibrateBool = False
+    videoLoaded = False
+    videoPlaying = False
     while True:
         event, values = window.read(timeout=20)
         if event == "Exit" or sg.WINDOW_CLOSED:
@@ -92,9 +125,11 @@ def main():
             plt.legend()
             plt.show()
             break
-        elif event == "Start":
-            testBool = True
-        elif testBool:
+        elif event == "Start cal":
+            calibrateBool = True
+        elif event == "Stop cal":
+            calibrateBool = False
+        elif calibrateBool:
             event, values = window.read(timeout=20)
             sf = values["-SCALE-"]/100
             mn = int(values["-NEIGHB-"])
@@ -103,24 +138,66 @@ def main():
             frame = cv2.flip(frame, 1)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             gray = cv2.equalizeHist(gray)
-            canvas = detect(gray, frame, sf, mn)
-            print(sf)
+            canvas = calibrate(gray, frame, sf, mn)
 
 
-            cv2.putText(canvas, 'Press q to stop', (50, 50), cv2.FONT_HERSHEY_SIMPLEX,
+            cv2.putText(canvas, 'Smile and adjust the scale factor', (50, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                        1, (0, 0, 0), 2, cv2.LINE_AA)
+            cv2.putText(canvas, 'till there is only one smile', (50, 80),
+                        cv2.FONT_HERSHEY_SIMPLEX,
                         1, (0, 0, 0), 2, cv2.LINE_AA)
             imgbytes = cv2.imencode(".png", canvas)[1].tobytes()
             window["-IMAGE-"].update(data=imgbytes)
-        elif testBool == False:
+
+        elif videoPlaying:
+            event, values = window.read(timeout=20)
+            sf = values["-SCALE-"]/100
+            mn = int(values["-NEIGHB-"])
             _, frame = cap.read()
             frame = cv2.resize(frame, (700, 512))
             frame = cv2.flip(frame, 1)
-            cv2.putText(frame, 'Press p to start', (50, 50), cv2.FONT_HERSHEY_SIMPLEX,
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = cv2.equalizeHist(gray)
+            detect(gray,frame,sf,mn)
+
+        elif not calibrateBool or videoLoaded:
+            _, frame = cap.read()
+            frame = cv2.resize(frame, (700, 512))
+            frame = cv2.flip(frame, 1)
+            cv2.putText(frame, 'Press start to calibrate', (50, 50), cv2.FONT_HERSHEY_SIMPLEX,
                         1, (0, 0, 0), 2, cv2.LINE_AA)
             # cv2.imshow('Video', frame)
             imgbytes = cv2.imencode(".png", frame)[1].tobytes()
             window["-IMAGE-"].update(data=imgbytes)
+        if event == 'play':
+            videoPlaying = True
+            list_player.play()
+        if event == 'pause':
+            list_player.pause()
+        if event == 'stop':
+            videoPlaying = False
+            list_player.stop()
+        if event == 'next':
+            list_player.next()
+            list_player.play()
+        if event == 'previous':
+            list_player.previous()  # first call causes current video to start over
+            list_player.previous()  # second call moves back 1 video from current
+            list_player.play()
+        if event == 'load':
+            if values['-VIDEO_LOCATION-'] and not 'Video URL' in values['-VIDEO_LOCATION-']:
+                videoLoaded = True
+                media_list.add_media(values['-VIDEO_LOCATION-'])
+                list_player.set_media_list(media_list)
+                window['-VIDEO_LOCATION-'].update('Video URL or Local Path:')  # only add a legit submit
 
+        # update elapsed time if there is a video loaded and the player is playing
+        if player.is_playing():
+            window['-MESSAGE_AREA-'].update(
+                "{:02d}:{:02d} / {:02d}:{:02d}".format(*divmod(player.get_time() // 1000, 60),
+                                                       *divmod(player.get_length() // 1000, 60)))
+        else:
+            window['-MESSAGE_AREA-'].update('Load media to start' if media_list.count() == 0 else 'Ready to play media')
 
 
 
